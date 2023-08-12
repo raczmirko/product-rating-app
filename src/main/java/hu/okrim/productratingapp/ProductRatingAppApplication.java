080431b6e6f1b09b391b5f79f634b0d9b735afbe
raczmirko <m1rk0d3v@gmail.com>
2023-08-12 16:44:53 +0200

    Initial commit

---
+++ src/main/java/hu/okrim/productratingapp/ProductRatingAppApplication.java
@@ -0,0 +1,13 @@
+package hu.okrim.productratingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRatingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/okrim/productratingapp/ProductRatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.okrim.productratingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
