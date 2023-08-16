06b51869c4aad598c3bc69c83bd70622dccfa0a1
raczmirko <m1rk0d3v@gmail.com>
2023-08-16 20:37:58 +0200

    feat: added a constants file for statuses

---
+++ src/main/java/hu/okrim/productratingapp/entity/Constants.java
@@ -0,0 +1,6 @@
+package hu.okrim.productratingapp.entity;
+
+public class Constants {
+    public static final String SUCCESS_STATUS = "success";
+    public static final String FAILED_STATUS = "failed";
+}
