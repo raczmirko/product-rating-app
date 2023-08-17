f77916f061d24ba40b8161a1ddef051bfec12c39
raczmirko <m1rk0d3v@gmail.com>
2023-08-17 11:25:07 +0200

    refactor: removed unused method and white spaces

--- src/main/java/hu/okrim/productratingapp/web/PersonController.java
+++ src/main/java/hu/okrim/productratingapp/web/PersonController.java
@@ -4,14 +4,10 @@ import hu.okrim.productratingapp.entity.Constants;
 import hu.okrim.productratingapp.entity.Person;
 import hu.okrim.productratingapp.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.List;
 
 @Controller
@@ -23,11 +19,6 @@ public class PersonController {
     public String listPeople(Model model) {
         List<Person> people = personService.getAllPeople();
         model.addAttribute("people", people);
-        return "people";
-    }
-    @GetMapping("/add-person")
-    public String showAddPersonForm(Model model) {
-        model.addAttribute("person", new Person());
         return "people";
     }
     @PostMapping("/add-person")
@@ -45,8 +36,4 @@ public class PersonController {
         redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
         return "redirect:/people";
     }
-//    @GetMapping
-//    public String getForm(Model model, @RequestParam(required = false) String id){
-//        return "people";
-//    }
 }
