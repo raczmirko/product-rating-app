90f449ec549c1dc3503f6deb4c200df7e52a79a9
raczmirko <m1rk0d3v@gmail.com>
2023-09-03 11:14:45 +0200

    refactor: removed unusued import

--- src/main/java/hu/okrim/productratingapp/web/ProductController.java
+++ src/main/java/hu/okrim/productratingapp/web/ProductController.java
@@ -7,7 +7,6 @@ import hu.okrim.productratingapp.entity.Product;
 import hu.okrim.productratingapp.service.BrandService;
 import hu.okrim.productratingapp.service.CategoryService;
 import hu.okrim.productratingapp.service.ProductService;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
