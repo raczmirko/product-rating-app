3b3ceaed1c1dcd87d4ac4166e742f60646599b9e
raczmirko <m1rk0d3v@gmail.com>
2023-09-03 12:20:40 +0200

    fix: fixed data type

--- src/main/java/hu/okrim/productratingapp/repository/ProductFlavourRepository.java
+++ src/main/java/hu/okrim/productratingapp/repository/ProductFlavourRepository.java
@@ -2,7 +2,8 @@ package hu.okrim.productratingapp.repository;
 
 import hu.okrim.productratingapp.entity.Person;
 import hu.okrim.productratingapp.entity.ProductFlavour;
+import hu.okrim.productratingapp.entity.ProductFlavourId;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductFlavourRepository extends CrudRepository<ProductFlavour, Integer> {
+public interface ProductFlavourRepository extends CrudRepository<ProductFlavour, ProductFlavourId> {
 }
