e05253520f2a5475c3ac39bf26c3f030ebfff4ae
raczmirko <m1rk0d3v@gmail.com>
2023-09-03 16:03:04 +0200

    fix: changed ID type to composite key object

--- src/main/java/hu/okrim/productratingapp/repository/RatingRepository.java
+++ src/main/java/hu/okrim/productratingapp/repository/RatingRepository.java
@@ -2,7 +2,8 @@ package hu.okrim.productratingapp.repository;
 
 import hu.okrim.productratingapp.entity.Person;
 import hu.okrim.productratingapp.entity.Rating;
+import hu.okrim.productratingapp.entity.RatingId;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RatingRepository extends CrudRepository<Rating, Integer> {
+public interface RatingRepository extends CrudRepository<Rating, RatingId> {
 }
