96ecd41080fa323f0dc2e6d0febfa9055a72b801
raczmirko <m1rk0d3v@gmail.com>
2023-09-07 23:18:49 +0200

    feat: added index page with particles.js vortex and dynamic query that shows latest 3 ratings

--- src/main/java/hu/okrim/productratingapp/web/IndexController.java
+++ src/main/java/hu/okrim/productratingapp/web/IndexController.java
@@ -1,6 +1,11 @@
 package hu.okrim.productratingapp.web;
 
+import hu.okrim.productratingapp.entity.Flavour;
+import hu.okrim.productratingapp.entity.Rating;
+import hu.okrim.productratingapp.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +14,18 @@ import java.util.List;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+    @Autowired
+    RatingService ratingService;
     @GetMapping("")
-    public String load() {
+    public String load(Model model) {
+        List<Rating> ratings = ratingService.getThreeLatestRatings();
+        model.addAttribute("ratings", ratings);
+        return "index";
+    }
+    @GetMapping("/get-latest-reviews")
+    public String loadLatestReviews(Model model){
+        List<Rating> ratings = ratingService.getThreeLatestRatings();
+        model.addAttribute("ratings", ratings);
         return "index";
     }
 }
