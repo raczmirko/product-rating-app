0ac378d1744d88596c8b09a057f70c7ae8722e28
raczmirko <m1rk0d3v@gmail.com>
2023-09-26 11:24:03 +0200

    feat: deleted tagInput.js since it is now replaced by tagController.js

--- src/main/resources/static/scripts/tagInput.js
+++ src/main/resources/static/scripts/tagInput.js
@@ -1,95 +1,97 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tagInput = document.getElementById('tag-input');
-    const selectedTags = document.getElementById('selected-tags');
-    let flavourDropdown = document.getElementById('flavours-dropdown'); // Declare flavourDropdown variable in the outer scope
-    var flavourList = [];
+  const tagInput = document.getElementById('tag-input');
+  const selectedTags = document.getElementById('selected-tags');
+  const flavourTable = document.getElementById('flavour-table').getElementsByTagName('tbody')[0];
+  let flavourList = [];
+  var pageSize = 6;
+  var pageNumber = 1;
 
-    // Function to fetch flavour and populate the dropdown
-    function populateDropdown() {
-      // Make a GET request to your API endpoint
-      fetch('/flavours/all')
-        .then((response) => response.json())
-        .then((data) => {
+  // Function to fetch flavours and populate the table
+  function populateTable() {
+    // Make a GET request to your API endpoint
+    fetch('/flavours/all')
+      .then((response) => response.json())
+      .then((data) => {
+        flavourList = data;
 
-          flavourList = data;
-          // Clear existing options
-          flavourDropdown.innerHTML = '';
+        // Clear the table body
+        flavourTable.innerHTML = '';
 
-          // Add a default option
-          const defaultOption = document.createElement('option');
-          defaultOption.value = '';
-          defaultOption.text = 'Select Flavor';
-          flavourDropdown.appendChild(defaultOption);
+        // Add matched flavours to the table
+        flavourList.forEach((flavour) => {
+          const row = flavourTable.insertRow();
+          const cell = row.insertCell(0);
+          cell.textContent = flavour.name;
 
-          // Add fetched flavour to the dropdown
-          data.forEach((flavours) => {
-            const option = document.createElement('option');
-            option.value = flavours.id;
-            option.text = flavours.name;
-            flavourDropdown.appendChild(option);
+          // Add a click event to select the option
+          row.addEventListener('click', () => {
+            selectFlavour(flavour.id);
           });
-        })
-        .catch((error) => {
-          console.error('Error fetching flavour:', error);
         });
-    }
-
-    function findFlavourName(flavourId) {
-      for (const element of flavourList) {
-        if(flavourId == element.id){
-            return element.name;
-        }
-      }
-      return 'Unknown';
-    }
-
-    // Function to add a selected tag
-    function addTag(tag) {
-      const selectedTag = document.createElement('div');
-      selectedTag.classList.add('selected-tag');
-      selectedTag.textContent = findFlavourName(tag);
-
-      // Add a click event to remove the tag when clicked
-      selectedTag.addEventListener('click', () => {
-        selectedTags.removeChild(selectedTag);
+      })
+      .catch((error) => {
+        console.error('Error fetching flavours:', error);
       });
+  }
 
-      selectedTags.appendChild(selectedTag);
-    }
+  function searchFlavours() {
+    const searchText = tagInput.value.toLowerCase();
+    const url = '/flavours/search?searchText=${searchText}&pageSize=${pageSize}&pageNumber=${pageNumber}';
+    // Make a GET request to your API endpoint
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        flavourList = data;
 
-    // Event listener for input field changes (e.g., user typing)
-    tagInput.addEventListener('input', () => {
-      const searchText = tagInput.value.toLowerCase();
-      // Filter flavour based on user input
-      const filteredFlavors = flavourList.filter((flavour) =>
-        flavour.name.toLowerCase().includes(searchText)
-      );
-      // Clear existing options
-      flavourDropdown.innerHTML = '';
-      // Add a default option
-      const defaultOption = document.createElement('option');
-      defaultOption.value = '';
-      defaultOption.text = 'Select Flavor';
-      flavourDropdown.appendChild(defaultOption);
-      // Add fetched flavour to the dropdown
-      filteredFlavors.forEach((filteredFlavor) => {
-      const option = document.createElement('option');
-      option.value = filteredFlavor.id;
-      option.text = filteredFlavor.name;
-      flavourDropdown.appendChild(option);
+        // Clear the table body
+        flavourTable.innerHTML = '';
+
+        // Add matched flavours to the table
+        flavourList.forEach((flavour) => {
+          const row = flavourTable.insertRow();
+          const cell = row.insertCell(0);
+          cell.textContent = flavour.name;
+
+          // Add a click event to select the option
+          row.addEventListener('click', () => {
+            selectFlavour(flavour.id);
+          });
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching flavours:', error);
       });
+  }
+
+  // Function to add a selected tag
+  function addTag(tagName) {
+    const selectedTag = document.createElement('div');
+    selectedTag.classList.add('selected-tag');
+    selectedTag.textContent = tagName;
+
+    // Add a click event to remove the tag when clicked
+    selectedTag.addEventListener('click', () => {
+      selectedTags.removeChild(selectedTag);
     });
 
-    // Event listener for selecting flavour from the dropdown
-    flavourDropdown.addEventListener('change', () => {
-      const selectedFlavor = flavourDropdown.value;
-      if (selectedFlavor) {
-        addTag(selectedFlavor);
-        populateDropdown();
-        tagInput.value = ''; // Clear the input field
-      }
-    });
+    selectedTags.appendChild(selectedTag);
+  }
 
-    // Initialize by populating the dropdown
-    populateDropdown();
+  // Function to select a flavour and add it as a tag
+  function selectFlavour(flavourId) {
+    const selectedFlavour = flavourList.find((flavour) => flavour.id === flavourId);
+    if (selectedFlavour) {
+      addTag(selectedFlavour.name);
+      tagInput.value = ''; // Clear the input field
+      flavourTable.innerHTML = ''; // Clear the table
+    }
+  }
+
+  // Event listener for input field changes (e.g., user typing)
+  tagInput.addEventListener('input', () => {
+    searchFlavours();
+  });
+
+  // Initialize by populating the table
+  populateTable();
 });
