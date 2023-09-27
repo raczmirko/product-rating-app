59bf603e3a0a8009369c95c1bc37f9c4b9b87b49
raczmirko <m1rk0d3v@gmail.com>
2023-09-27 13:19:34 +0200

    fix: fixed pagination, removed unused elements

--- src/main/resources/static/scripts/flavourTagController.js
+++ src/main/resources/static/scripts/flavourTagController.js
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchBar = document.getElementById('tag-input');
     const pageSizeSelect = document.getElementById('items-per-page-select');
     const form = document.querySelector('form');
-    let searchText = searchBar.value != null ? searchBar.value.toLowerCase() : null;
-    let flavoursParamString = '';
+    let searchText = '';
     let flavours = []; // Store flavour data here
     let pageNumber = 1;
     let pageSize = parseInt(pageSizeSelect.value); // Parse the initial value
+    let totalElements = 0;
     // Define an array to store selected flavors (tags)
     const selectedFlavors = [];
     const selectedFlavorIDs = [];
-    let url = `/flavours/search?searchText=${searchText}&pageSize=${pageSize}&pageNumber=${pageNumber}&flavours=${flavoursParamString}`;
+    let url = `/flavours/search?searchText=${searchText}&pageSize=${pageSize}&pageNumber=${pageNumber}`;
 
     //------------------------------------|
     //-----------Method calls-------------|
@@ -32,87 +32,85 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to render flavour cards
     function renderFlavorCards(page) {
-    flavourContainer.innerHTML = ''; // Clear previous cards
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
-    const flavourPage = flavours.slice(startIndex, endIndex);
+        flavourContainer.innerHTML = ''; // Clear previous cards
 
-    flavourPage.forEach((flavour) => {
-      const card = document.createElement('div');
-      card.classList.add('flavour-card');
-      card.textContent = flavour.name;
-      flavourContainer.appendChild(card);
-    });
+        flavours.forEach((flavour) => {
+          const card = document.createElement('div');
+          card.classList.add('flavour-card');
+          card.textContent = flavour.name;
+          flavourContainer.appendChild(card);
+        });
     }
 
     // Function to update pagination controls
     function updatePagination() {
-    pagination.innerHTML = '';
+        pagination.innerHTML = '';
 
-    const totalPages = Math.ceil(flavours.length / pageSize);
-    const buttonsToShow = 3; // Number of buttons to show in pagination
+        const totalPages = Math.ceil(totalElements / pageSize);
+        const buttonsToShow = 3; // Number of buttons to show in pagination
 
-    // Calculate the range of page numbers to display
-    let startPage = pageNumber - Math.floor(buttonsToShow / 2);
-    let endPage = pageNumber + Math.floor(buttonsToShow / 2);
+        // Calculate the range of page numbers to display
+        let startPage = pageNumber - Math.floor(buttonsToShow / 2);
+        let endPage = pageNumber + Math.floor(buttonsToShow / 2);
 
-    // Ensure the range is within valid page numbers
-    if (startPage < 1) {
-      startPage = 1;
-      endPage = Math.min(totalPages, buttonsToShow);
-    } else if (endPage > totalPages) {
-      endPage = totalPages;
-      startPage = Math.max(1, totalPages - buttonsToShow + 1);
-    }
+        // Ensure the range is within valid page numbers
+        if (startPage < 1) {
+          startPage = 1;
+          endPage = Math.min(totalPages, buttonsToShow);
+        } else if (endPage > totalPages) {
+          endPage = totalPages;
+          startPage = Math.max(1, totalPages - buttonsToShow + 1);
+        }
 
-    // Helper function to create a pagination button
-    function createPaginationButton(text, page) {
-      const pageLink = document.createElement('span');
-      pageLink.textContent = text;
-      pageLink.classList.add('page-link');
-      if (page === pageNumber) {
-        pageLink.classList.add('active');
-      }
-      pageLink.addEventListener('click', () => {
-        pageNumber = page;
-        renderFlavorCards(pageNumber);
-        updatePagination();
-      });
-      pagination.appendChild(pageLink);
-    }
+        // Helper function to create a pagination button
+        function createPaginationButton(text, page) {
+          const pageLink = document.createElement('span');
+          pageLink.textContent = text;
+          pageLink.classList.add('page-link');
+          if (page === pageNumber) {
+            pageLink.classList.add('active');
+          }
+          pageLink.addEventListener('click', () => {
+            pageNumber = page;
+            searchFlavours();
+          });
+          pagination.appendChild(pageLink);
+        }
 
-    // Create the << button to go to the first page
-    if (pageNumber > 1) {
-      createPaginationButton('<<', 1);
-    }
+        // Create the << button to go to the first page
+        if (pageNumber > 1) {
+          createPaginationButton('<<', 1);
+        }
 
-    // Create the < button to go one page back
-    if (pageNumber > 1) {
-      createPaginationButton('<', pageNumber - 1);
-    }
+        // Create the < button to go one page back
+        if (pageNumber > 1) {
+          createPaginationButton('<', pageNumber - 1);
+        }
 
-    // Create page buttons for the range
-    for (let page = startPage; page <= endPage; page++) {
-      createPaginationButton(page.toString(), page);
-    }
+        // Create page buttons for the range
+        for (let page = startPage; page <= endPage; page++) {
+          createPaginationButton(page.toString(), page);
+        }
 
-    // Create the > button to go one page forward
-    if (pageNumber < totalPages) {
-      createPaginationButton('>', pageNumber + 1);
-    }
+        // Create the > button to go one page forward
+        if (pageNumber < totalPages) {
+          createPaginationButton('>', pageNumber + 1);
+        }
 
-    // Create the >> button to go to the last page
-    if (pageNumber < totalPages) {
-      createPaginationButton('>>', totalPages);
-    }
+        // Create the >> button to go to the last page
+        if (pageNumber < totalPages) {
+          createPaginationButton('>>', totalPages);
+        }
     }
 
     function searchFlavours() {
         searchText = searchBar.value != null ? searchBar.value.toLowerCase() : null;
+        url = `/flavours/search?searchText=${searchText}&pageSize=${pageSize}&pageNumber=${pageNumber}`;
          fetch(url)
           .then((response) => response.json())
           .then((data) => {
           flavours = data.content;
+          totalElements = data.totalElements;
           renderFlavorCards(pageNumber);
           updatePagination();
         })
@@ -206,11 +204,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Listen for changes in the <select> element
     pageSizeSelect.addEventListener('change', function () {
-    // Update the pageSize variable with the selected value
-    pageSize = parseInt(pageSizeSelect.value); // Parse the value as an integer
-    pageNumber = 1; // Reset to the first page when changing items per page
-    renderFlavorCards(pageNumber); // Re-render cards based on the new items per page
-    updatePagination(); // Update pagination controls
+        // Update the pageSize variable with the selected value
+        pageSize = parseInt(pageSizeSelect.value); // Parse the value as an integer
+        pageNumber = 1; // Reset to the first page when changing items per page
+        searchFlavours();
     });
 
     // Event listener for form submission
