782700003f926be949dcca8389def1690f80e853
raczmirko <m1rk0d3v@gmail.com>
2023-09-30 12:42:35 +0200

    feat: added a popup window that looks nicer than the default browser confirmation window

--- src/main/resources/static/scripts/confirmDelete.js
+++ src/main/resources/static/scripts/confirmDelete.js
@@ -1,3 +1,29 @@
-function confirmDelete(deletionObjectType) {
-    return confirm("Are you sure you want to delete this " + deletionObjectType + "?");
+//--------------------------------------------------------------|
+//-----Script for handling the popup window functionality------//
+//--------------------------------------------------------------|
+function showModal(thingToDelete) {
+    // Get the modal
+    var modal = document.getElementById("modal");
+    // Get the text on modal
+    // Get a reference to the message element by its id
+    const popupMessage = document.getElementById("popup-message");
+    popupMessage.textContent  = "Are you sure want to delete this".concat(" ", thingToDelete, "?");
+    // When the user clicks the button, open the modal
+    modal.style.display = "block";
+    // Get the <span> element that closes the modal
+    var cancelButton = document.getElementById("modal-no");
+    var confirmButton = document.getElementById("modal-no");
+    confirmButton.onclick = function() {
+        var deletionForm = document.getElementById("deletion-form");
+        deletionForm.submit();
+    }
+    cancelButton.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
