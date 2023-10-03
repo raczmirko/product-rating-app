e5964af4bf908368472e202b43419fc6ed9ffd5b
raczmirko <m1rk0d3v@gmail.com>
2023-10-03 08:42:53 +0200

    feat: changed the deletion modal text

--- src/main/resources/static/scripts/confirmDelete.js
+++ src/main/resources/static/scripts/confirmDelete.js
@@ -1,20 +1,19 @@
 //--------------------------------------------------------------|
 //-----Script for handling the popup window functionality------//
 //--------------------------------------------------------------|
-function showModal(thingToDelete, index) {
+function showModal(objectType, thingToDelete, index) {
     // Get the modal
     var modal = document.getElementById("modal");
     // Get the text on modal
     // Get a reference to the message element by its id
     const popupMessage = document.getElementById("popup-message");
-    popupMessage.textContent  = "Are you sure want to delete ".concat(" '", thingToDelete, "'?");
+    popupMessage.textContent  = "Are you sure want to delete the following ".concat(objectType, ": ",thingToDelete, "?");
     // When the user clicks the button, open the modal
     modal.style.display = "block";
     // Get the <span> element that closes the modal
     var cancelButton = document.getElementById("modal-no");
     var confirmButton = document.getElementById("modal-yes");
     confirmButton.onclick = function() {
-        console.log("Just the element: ", document.getElementById("productIndex"));
         const formID = "deletion-form-".concat(index.current.id);
         console.log(formID)
         const deletionForm = document.getElementById(formID);
