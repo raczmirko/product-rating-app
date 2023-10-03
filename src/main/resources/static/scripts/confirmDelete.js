d432d5927d8a5e5757d778b5b043f8efd0ac566b
raczmirko <m1rk0d3v@gmail.com>
2023-10-03 09:33:15 +0200

    feat: added a showModalCompositeKey method to the confirmDeletee controller which uses a different approach to passing the ID of an element, for elements with composite keys

--- src/main/resources/static/scripts/confirmDelete.js
+++ src/main/resources/static/scripts/confirmDelete.js
@@ -29,3 +29,31 @@ function showModal(objectType, thingToDelete, index) {
         }
     }
 }
+
+function showModalCompositeKey(objectType, thingToDelete, ID) {
+    // Get the modal
+    var modal = document.getElementById("modal");
+    // Get the text on modal
+    // Get a reference to the message element by its id
+    const popupMessage = document.getElementById("popup-message");
+    popupMessage.textContent  = "Are you sure want to delete the following ".concat(objectType, ": ",thingToDelete, "?");
+    // When the user clicks the button, open the modal
+    modal.style.display = "block";
+    // Get the <span> element that closes the modal
+    var cancelButton = document.getElementById("modal-no");
+    var confirmButton = document.getElementById("modal-yes");
+    confirmButton.onclick = function() {
+        console.log(ID)
+        const deletionForm = document.getElementById(ID);
+        deletionForm.submit();
+    }
+    cancelButton.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
